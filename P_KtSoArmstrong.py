ba616ede58d973e193e845423bad80aa27f5d794
TamNhien <canhthien91@gmail.com>
2024-03-29 19:15:40 +0700

    Add project files.

---
+++ P_KtSoArmstrong.py
@@ -0,0 +1,9 @@
+﻿def is_armstrong(num):
+    return num == sum(int(digit) ** len(str(num)) for digit in str(num))
+
+num = int(input("Nhập số nguyên: "))
+if is_armstrong(num):
+    print(f"{num} là số Armstrong")
+else:
+    print(f"{num} không phải là số Armstrong")
+    
